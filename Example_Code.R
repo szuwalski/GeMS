c6a9a886d12a6440561c94b49cfaa4e5983fceb7
Lee Qi <leeqi@ucsb.edu>
2018-05-04 15:22:20 -0700

    Readme

--- Example_Code.R
+++ Example_Code.R
@@ -1,45 +1,22 @@
 ##----------------
 ## Run, GeMS, run!
 ##----------------
-
-# Set directory where GeMS is located
-dir.GeMS <- "~/Box\ Sync/GeMS/General-MSE"
+# Load package
+library(GeMS)
 
 # Set directory where control files are
-#dir.MSE <- ""~/Box\ Sync/GeMS/General-MSE"/TestHCRs"
-dir.MSE <- file.path(dir.GeMS, "TestHCRs")
+dir.MSE <- system.file("Cod_1_Production",package="GeMS") #Located in the Examples/ folder
 
 # Names of control files to be tested
-OMNames<-c("Cod_AgeStructure_CTL_1","Cod_AgeStructure_CTL_2","Cod_AgeStructure_CTL_3","Cod_AgeStructure_CTL_4")
-
-# Loading the wrapper function
-source(file.path(dir.GeMS,"run_GeMS.R"))
+OMNames<-c("Cod_LowProd_CTL", "Cod_Base_CTL", "Cod_HighProd_CTL")
 
 # Beginning the MSE
-# GeMSDir is the directory where GeMS files are
-# CurDir is the directory where control files are
-# CreateFolderNameList is the vector of control file names
-# GeMSops is an optional named list of options used in the GeMS() function
-#	silent = T sends ADMB console output into a console.log text file
-#	Adoptions = character string of ADMB options, e.g. -nohess, -gbs, -cbs etc.
-#	runparallel = T runs control files in parallel
-#	cores = integer stating number of cores to use in parallel processing
-# shutup is a T/F option to display run_GeMS() messages
+run_GeMS(OMNames,dir.MSE)
 
-run_GeMS(GeMSDir=dir.GeMS, CurDir=dir.MSE,
-		 CreateFolderNameList=OMNames)
-
-##----------------------
-## Example using GeMSops
-##----------------------
-#run_GeMS(GeMSDir=dir.GeMS, CurDir=dir.MSE,
-#		 CreateFolderNameList=OMNames,
-#		 GeMSops = list(silent = T, ADoptions = "-gbs 2000000000"))
+# Plots can then be found at file.path(dir.MSE, "plots")
 
 ##-----------------
 ## Parallel example
 ##-----------------
-#run_GeMS(GeMSDir=dir.GeMS, CurDir=dir.MSE,
-#		 CreateFolderNameList=OMNames,
-#		 GeMSops = list(silent = T, ADoptions = "-gbs 2000000000")
-#	     runparallel = T, cores = 4)
+#run_GeMS(OMNames,dir.MSE,
+#	        runparallel = T, cores = 3)
--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 # General-MSE
 A 2-box, generalized management strategy evaluation framework that allows time-varying processes.
 See the wiki!
+
+### Installation
+To install the `GeMS` package, the [`devtools`](https://www.rstudio.com/products/rpackages/devtools/) package is required.
+
+```
+install.packages("devtools")
+devtools::install_github("szuwalski/GeMS")
+```
+
+### Code Example
+```
+##----------------
+## Run, GeMS, run!
+##----------------
+# Load package
+library(GeMS)
+
+# Set directory where control files are
+dir.MSE <- system.file("Cod_1_Production",package="GeMS") #Located in the Examples/ folder of the GeMS directory
+
+# Names of control files to be tested
+OMNames<-c("Cod_LowProd_CTL", "Cod_Base_CTL", "Cod_HighProd_CTL")
+
+# Beginning the MSE
+run_GeMS(OMNames,dir.MSE)
+
+# Plots can then be found at file.path(dir.MSE, "plots")
+
+##-----------------
+## Parallel example
+##-----------------
+# run_GeMS(OMNames,dir.MSE,
+#	        runparallel = T, cores = 3)
+```
